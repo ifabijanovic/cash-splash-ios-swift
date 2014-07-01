dbac3ed329470e3b258b61ca5cd2a145eaf1ea6e
Ivan Fabijanovic <feb@shoutem.com>
2014-07-01 11:31:56 +0200

    Added a new Empty App project.

---
+++ CashSplash/CashSplashTests/CashSplashTests.swift
@@ -0,0 +1,35 @@
+//
+//  CashSplashTests.swift
+//  CashSplashTests
+//
+//  Created by Ivan Fabijanovic on 01/07/14.
+//  Copyright (c) 2014 Ivan Fabijanovic. All rights reserved.
+//
+
+import XCTest
+
+class CashSplashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
